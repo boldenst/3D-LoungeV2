a31aac242aa6a251508650525390b2bcd7eb2b29
Martin Windsor <martinwindsor1@gmail.com>
2021-10-03 19:12:56 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -11,13 +11,7 @@ import './assets/tailwind.css'
 import * as firebase from "firebase"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAR-83y1xk0JmgZ-A-D8z_Y_xyoFEhRPiI",
-  authDomain: "d-lounge-e177a.firebaseapp.com",
-  projectId: "d-lounge-e177a",
-  storageBucket: "d-lounge-e177a.appspot.com",
-  messagingSenderId: "13265840535",
-  appId: "1:13265840535:web:c71fd3319b74bf0816bcd2",
-  measurementId: "G-S31NX5V1HM"
+
 };
 
 
