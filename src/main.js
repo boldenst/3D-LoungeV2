49dc12a70356442409e42bf20d45d9ffc2d0434b
Martin Windsor <martinwindsor1@gmail.com>
2021-10-04 22:02:08 +0200

    Blank page bug

--- src/main.js
+++ src/main.js
@@ -1,17 +1,31 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import { initializeApp } from 'firebase/app';
+
 import jQuery from 'jquery'
 global.jQuery = jQuery
 global.$ = jQuery
 let Bootstrap = require('bootstrap')
 import 'bootstrap/dist/css/bootstrap.css'
+
 import './assets/tailwind.css'
 
 Vue.config.productionTip = false
+
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_APP_ID,
+  measurementId: process.env.VUE_APP_MEASUREMENT_ID
+};
+
+const app = initializeApp(firebaseConfig);
 
 new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
-
