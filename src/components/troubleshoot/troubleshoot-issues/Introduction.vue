9545c3f2ae621a31393e249d32c0c268977ab713
Oliver Hansen <oliv045r@edu.easj.dk>
2021-10-05 17:34:25 +0200

    sadsad

--- src/components/troubleshoot/troubleshoot-issues/Introduction.vue
+++ src/components/troubleshoot/troubleshoot-issues/Introduction.vue
@@ -2,34 +2,46 @@
     <section class="issue-intro">
         <slot name="issue-heading">
             <h1 class="issue-heading">ISSUE HEADING</h1>
-            <div class="issue-content">
-                <div class="issue-content__desc">
-                    <div class="issue-content-desc__tags">
+        </slot>
+        <div class="issue-content">
+            <div class="issue-content__desc">
+                <div class="issue-content-desc__tags">
+                    <slot name="issue-tags">
                         <p class="issue-content-desc-tags__tag main-tag">Tags:</p>
                         <p class="issue-content-desc-tags__tag">Tag,</p>
                         <p class="issue-content-desc-tags__tag">Tag,</p>
                         <p class="issue-content-desc-tags__tag">Tag</p>
-                    </div>
-                    <div class="icons-seperator">
+                    </slot>
+
+                </div>
+                <div class="icons-seperator">
+                    <slot name="issue-introdesc">
                         <p class="issue-content-desc_text">
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
                             labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
                             laboris nisi ut aliquip ex ea commo labore et dolore magna aliqua.
                         </p>
-                        <div class="issue-content-desc_icons">
-                            <router-link to="#"><img src="@/assets/Home.svg" alt=""></router-link>
-                            <router-link to="#"><img src="@/assets/Home.svg" alt=""></router-link>
-                            <router-link to="#"><img src="@/assets/Home.svg" alt=""></router-link>
-                        </div>
+                    </slot>
+
+                    <div class="issue-content-desc__icons">
+                        <router-link class="issue-content-desc-icons__icon" to="#"><img src="@/assets/icons/Share.svg"
+                                alt=""></router-link>
+                        <router-link class="issue-content-desc-icons__icon" to="#"><img src="@/assets/icons/Edit.svg"
+                                alt=""></router-link>
+                        <router-link class="issue-content-desc-icons__icon" to="#"><img src="@/assets/icons/Star.svg"
+                                alt=""></router-link>
                     </div>
                 </div>
-                <div class="issue-content__gallery">
+            </div>
+            <div class="issue-content__gallery">
+                <slot name="issue-gallery">
                     <div class="issue-content-gallery__image-main"></div>
                     <div class="issue-content-gallery__image-second"></div>
                     <div class="issue-content-gallery__image-third"></div>
-                </div>
+                </slot>
+
             </div>
-        </slot>
+        </div>
     </section>
 
 </template>
--- src/views/troubleshoot/troubleshoot-issues/issue-2.vue
+++ src/views/troubleshoot/troubleshoot-issues/issue-2.vue
@@ -1,8 +1,42 @@
 <template>
   <div id="issue1">
     <Breadcrumb></Breadcrumb>
-    <Introduction></Introduction>
-    <Causes></Causes>
+    <Introduction>
+      <template v-slot:issue-heading>
+        <h1 class="issue-heading">Layer Adhesion Issues</h1>
+      </template>
+      <template v-slot:issue-tags>
+        <p class="issue-content-desc-tags__tag main-tag">Tags:</p>
+        <p class="issue-content-desc-tags__tag">Lines,</p>
+        <p class="issue-content-desc-tags__tag">Breaks,</p>
+        <p class="issue-content-desc-tags__tag">Seperates</p>
+      </template>
+      <template v-slot:issue-introdesc>
+        <p class="issue-content-desc_text">
+          Layer adhesion issues can cause a lot of issues with strength etc. ipsum dolor sit amet, consectetur
+          adipiscing elit, sed do eiusmod tempor incididunt ut
+          labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+          laboris nisi ut aliquip ex ea commo labore et dolore magna aliqua.
+        </p>
+      </template>
+      <template v-slot:issue-gallery>
+        <div class="issue-content-gallery__image-main"></div>
+        <div class="issue-content-gallery__image-second"></div>
+        <div class="issue-content-gallery__image-third"></div>
+      </template>
+    </Introduction>
+    <Causes>
+      <template v-slot:causes>
+        <p class="causes">Too low temp</p>
+        <p class="causes">Underextrusion</p>
+      </template>
+      <template v-slot:solutions>
+        <p class="solutions">Increase temp <router-link to="/SignIn">Learn how...</router-link>
+        </p>
+        <p class="solutions">Increase extrusion <router-link to="/SignIn">Learn how...</router-link>
+        </p>
+      </template>
+    </Causes>
     <Explanations></Explanations>
     <Explanations></Explanations>
     <Comments></Comments>
@@ -14,37 +48,42 @@
 
 <style scoped>
   #issue1 {
-
+    margin: 5rem 14vw;
   }
 
-  @media only screen and (min-width: 1000px) {
-  #issue1 {
-    margin: 0.5rem 14vw;
-    padding: 2rem 0rem;
+  .issue-content-gallery__image-main {
+    background-image: url(../../../assets/issue1-1.png);
+    background-size: 120%;
+    background-position: center;
   }
-}
 
+  .issue-content-gallery__image-second {
+    background-image: url(../../../assets/issue1-2.png);
+    background-size: 125%;
+    background-position: center;
+  }
 
-
-
+  .issue-content-gallery__image-third {
+    background-image: url(../../../assets/issue1-3.png);
+    background-size: 115%;
+    background-position: center;
+  }
 </style>
 
 <script>
-// @ is an alias to /src
-import Introduction from '@/components/troubleshoot/troubleshoot-issues/Introduction.vue'
-import Causes from '@/components/troubleshoot/troubleshoot-issues/Causes.vue'
-import Explanations from '@/components/troubleshoot/troubleshoot-issues/Explanations.vue'
-import Comments from '@/components/troubleshoot/troubleshoot-issues/Comments.vue'
+  // @ is an alias to /src
+  import Introduction from '@/components/troubleshoot/troubleshoot-issues/Introduction.vue'
+  import Causes from '@/components/troubleshoot/troubleshoot-issues/Causes.vue'
+  import Explanations from '@/components/troubleshoot/troubleshoot-issues/Explanations.vue'
+  import Comments from '@/components/troubleshoot/troubleshoot-issues/Comments.vue'
 
-export default {
-  name: 'App',
-  components: {
-    Introduction,
-    Causes,
-    Explanations,
-    Comments
+  export default {
+    name: 'App',
+    components: {
+      Introduction,
+      Causes,
+      Explanations,
+      Comments
+    }
   }
-}
 </script>
-
-    
