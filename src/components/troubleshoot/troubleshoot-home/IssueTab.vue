731d26d9aa086c851315bdc53002c1db3faf5d11
Hjorthex <74530828+Hjorthex@users.noreply.github.com>
2021-10-08 08:55:32 +0200

    jghh

---
+++ src/components/troubleshoot/troubleshoot-home/IssueTab.vue
@@ -0,0 +1,21 @@
+<template>
+    <div v-if="isSelected">
+        <h2>sup</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        isSelected: {
+            type: Boolean
+        }
+    }
+}
+
+
+</script>
+
+<style scoped>
+
+</style>
--- src/views/troubleshoot/Troubleshoot.vue
+++ src/views/troubleshoot/Troubleshoot.vue
@@ -2,7 +2,20 @@
   <div id="issue1">
     <Description> </Description>
     <FilterSearch></FilterSearch>
-    <CommonIssues></CommonIssues>
+    <IssueNav :tabs="['Issue1', 'Issue2', 'Issue3']" :selected="selected" @selected="setSelected">
+        <Tab :isSelected="selected === 'Issue1'">
+            <h2>Issue1</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, nostrum?</p>
+        </Tab>
+        <Tab :isSelected="selected === 'Issue2'">
+            <h2>Issue2</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, nostrum?</p>
+        </Tab>
+        <Tab :isSelected="selected === 'Issue3'">
+            <h2>Issue3</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, nostrum?</p>
+        </Tab>
+    </IssueNav>
   </div>
 </template>
 
@@ -10,16 +23,35 @@
 // @ is an alias to /src
 import Description from '@/components/troubleshoot/troubleshoot-home/Description.vue'
 import FilterSearch from '@/components/troubleshoot/troubleshoot-home/FilterSearch.vue'
-import CommonIssues from '@/components/troubleshoot/troubleshoot-home/CommonIssues'
+import IssueNav from '@/components/troubleshoot/troubleshoot-home/IssueNav.vue'
+import IssueTab from '@/components/troubleshoot/troubleshoot-home/IssueTab.vue'
 
 export default {
   name: 'Troubleshoot',
   components: {
     Description,
     FilterSearch,
-    CommonIssues
+    IssueNav,
+    IssueTab
+  },
+  data() {
+    return {
+      selected: 'Issue1'
+    }
+  },
+  methods: {
+    setSelected(tab) {
+      this.selected = tab;
+    }
   }
 }
 </script>
 
-    
+<style scoped>
+
+h2 {
+  font-size: 1.5rem;
+  color: green;
+}
+
+</style>
